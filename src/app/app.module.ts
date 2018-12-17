0c3c30d292aad37b8900449cd7f4eb63f4731e7a
erhankocabuga <erhankocabuga@gmail.com>
2018-12-17 16:41:59 +0300

    Add news pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
