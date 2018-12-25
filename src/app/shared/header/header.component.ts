630d369a58084414930494c70c389a4db9352d26
erhankocabuga <erhankocabuga@gmail.com>
2018-12-25 17:18:09 +0300

    HTML düzenlemeleri

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import Utils from '../../general/utils';
+import { Section } from '../../models/Objects';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+ 
+  categories: Section[] = Utils.getCategories();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getSectionClass(sectionId:string): string {
+    return Utils.getSectionClassName(sectionId);
+  }
 }
